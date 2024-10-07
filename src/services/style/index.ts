0e07f09008e09136c65b1b8c7f3816252e095653
kvarela <karim@metalink.com>
2024-10-07 11:26:54 -0700

    Updated StyleService

--- src/services/style/index.ts
+++ src/services/style/index.ts
@@ -1,8 +1,38 @@
 import { AxiosInstance } from 'axios'
 import { IStyle } from './style.interface'
+import { ICreateCustomStyleRequest } from './create-custom-style-request.interface'
 
 export class StyleService {
+  private static ENTITY = 'styles'
+  private static CUSTOM = 'custom'
+
   constructor(private readonly axios: AxiosInstance) {}
+
+  /**
+   * Kicks off the process of creating a custom style.
+   * @param params - The parameters for creating a custom style.
+   * @returns A Promise that resolves to the created style.
+   */
+  createCustomStyle = async (params: ICreateCustomStyleRequest) => {
+    const { data } = await this.axios.post(`/${StyleService.ENTITY}`, params, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+
+    return data
+  }
+
+  /**
+   * Retrieves all custom styles.
+   * @returns A Promise that resolves to an array of styles.
+   */
+  getCustomStyles = async () => {
+    const { data } = await this.axios.get<IStyle[]>(
+      `/${StyleService.ENTITY}/${StyleService.CUSTOM}`
+    )
+    return data
+  }
 
   /**
    * Retrieves all styles based on the provided template ID and pinned status.
@@ -12,7 +42,7 @@ export class StyleService {
    */
   getAll = async (templateId?: string, isPinned?: boolean): Promise<IStyle[]> => {
     const { data } = await this.axios.get<IStyle[]>(
-      `/styles${templateId ? `?templateId=${templateId}` : ''}${
+      `/${StyleService.ENTITY}${templateId ? `?templateId=${templateId}` : ''}${
         isPinned ? `&isPinned=${isPinned}` : ''
       }`
     )
