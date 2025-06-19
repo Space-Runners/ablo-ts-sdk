e9b7a700bf611ac764cf78e4024638867c7ef8b0
Okan Aslan <okanaslan.08@gmail.com>
2025-06-19 14:26:05 +0300

    Add optional cache control config to storage upload function

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,30 +1,32 @@
-import { AxiosInstance } from 'axios'
+import { AxiosHeaders, AxiosInstance } from 'axios'
 
 export class StorageService {
-  constructor(private readonly axios: AxiosInstance) {}
+  constructor(private readonly axios: AxiosInstance) { }
 
   /**
    * Uploads an image from a base64 string to the storage service.
    * @param image - The base64 encoded image.
    * @param contentType - The content type of the image.
+   * @param options - Optional parameters for the upload, such as cache control.
    * @returns A Promise that resolves to the URL of the uploaded image.
    */
-  uploadBase64 = async (image: string, contentType: string): Promise<string> => {
+  uploadBase64 = async (image: string, contentType: string, options?: { cacheControl?: string }): Promise<string> => {
     const url = await this.getSignedUrl(contentType)
     const blob = await fetch(image).then((res) => res.blob())
-    return this.upload(url, blob as unknown as string, contentType)
+    return this.upload(url, blob as unknown as string, contentType, options)
   }
 
   /**
    * Uploads an image to the storage service.
    * @param image - The image to be uploaded.
    * @param contentType - The content type of the image.
+   * @param options - Optional parameters for the upload, such as cache control.
    * @returns A Promise that resolves to the URL of the uploaded image.
    */
-  uploadBlob = async (image: File | ArrayBuffer | string, contentType: string): Promise<string> => {
+  uploadBlob = async (image: File | ArrayBuffer | string, contentType: string, options?: { cacheControl?: string }): Promise<string> => {
     const url = await this.getSignedUrl(contentType)
 
-    return this.upload(url, image, contentType)
+    return this.upload(url, image, contentType, options)
   }
 
   /**
@@ -42,16 +44,22 @@ export class StorageService {
    * @param url - The URL to upload the image to.
    * @param image - The image to be uploaded.
    * @param contentType - The content type of the image.
+   * @param options - Optional parameters for the upload, such as cache control.
    * @returns A Promise that resolves to the URL of the uploaded image.
    */
   upload = async (
     url: string,
     image: File | ArrayBuffer | string,
-    contentType: string
+    contentType: string,
+    options?: { cacheControl?: string }
   ): Promise<string> => {
-    await this.axios.put(url, image, {
-      headers: { 'Content-Type': contentType },
-    })
+    const headers = new AxiosHeaders()
+    headers.append('Content-Type', contentType)
+    if (options?.cacheControl) {
+      headers.append('Cache-Control', options.cacheControl)
+    }
+
+    await this.axios.put(url, image, { headers: headers })
     return url.split('?')[0]
   }
 }
